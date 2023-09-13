34b65000d6c6ac54d3d49a1ef36c560bf1edeae9
Aravindan Vasudevan <aravindan.vasudevan98@gmail.com>
2023-09-12 20:20:00 -0500

    adding  files

--- model.py
+++ model.py
@@ -1,34 +1,59 @@
 import torch
+import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.distributions import Normal
 from hyperparameter import device
 
+# class Policy(nn.Module):
+    
+#     def __init__(self, state_size, action_size):
+#         self.state_size = state_size
+#         self.action_size = action_size
+#         super(Policy, self).__init__()
+#         self.fc1 = nn.Linear(state_size, 128)
+#         self.fc2 = nn.Linear(128, action_size)
+
+#     def forward(self, x):
+#         x = F.relu(self.fc1(x))
+#         action_probs = F.softmax(self.fc2(x), dim = -1)
+#         return action_probs
+    
+#     def act(self, state):
+#         state = torch.from_numpy(state).float().unsqueeze(0).to(device)
+#         action_probs = self.forward(state)
+#         highest_prob_action = np.random.choice(self.action_size, p = np.squeeze(action_probs.detach().cpu().numpy()))
+#         log_prob = torch.log(action_probs.squeeze(0)[highest_prob_action])
+#         return highest_prob_action, log_prob
+
 class Policy(nn.Module):
     
     def __init__(self, state_size, action_size):
+        self.state_size = state_size
+        self.action_size = action_size
         super(Policy, self).__init__()
-        self.fc1 = nn.Linear(state_size, 32)
-        self.fc2 = nn.Linear(32, 128)
-        self.fc3 = nn.Linear(128, 256)
-        self.fc4 = nn.Linear(256, 64)
-        self.fc5 = nn.Linear(64, action_size * 2)
+        self.fc1 = nn.Linear(state_size, 128)
+        self.fc2 = nn.Linear(128, 256)
+        self.fc3 = nn.Linear(256, 512)
+        self.fc4 = nn.Linear(512, 128)
+        self.fc5_mean = nn.Linear(128, action_size) 
+        self.fc5_stddev = nn.Linear(128, action_size)
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = F.relu(self.fc3(x))
         x = F.relu(self.fc4(x))
-        x = self.fc5(x)
-        return x
+        mean = torch.tanh(self.fc5_mean(x))
+        stddev = F.softplus(self.fc5_stddev(x))
+        return mean, stddev
     
     def act(self, state):
         state = torch.from_numpy(state).float().unsqueeze(0).to(device)
-        action_params = self.forward(state).cpu()
+        mean, stddev = self.forward(state)
+        # Sample an action from a Gaussian distribution for each dimension
+        action = torch.normal(mean, stddev)
+        action_dist = Normal(mean, stddev)
+        log_prob = action_dist.log_prob(action)
 
-        mean, log_std = torch.chunk(action_params, 2, dim = 1)
-        action_dist = Normal(mean, torch.exp(log_std))
-        action = action_dist.sample()
-        log_prob = action_dist.log_prob(action).sum(dim = -1)
-
-        return action.numpy()[0], log_prob.item()
+        return action.squeeze().detach().cpu().numpy(), log_prob.sum().item()
